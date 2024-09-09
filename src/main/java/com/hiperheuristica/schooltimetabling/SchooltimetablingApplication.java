e36e0fa7f41b3edddc921853fcb69c27baee5855
Brener Lima <brener_llb@hotmail.com>
2024-09-09 20:24:43 -0300

    Criação do projeto

---
+++ src/main/java/com/hiperheuristica/schooltimetabling/SchooltimetablingApplication.java
@@ -0,0 +1,13 @@
+package com.hiperheuristica.schooltimetabling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooltimetablingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooltimetablingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiperheuristica/schooltimetabling/SchooltimetablingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiperheuristica.schooltimetabling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooltimetablingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
